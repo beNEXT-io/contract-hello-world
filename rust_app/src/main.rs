afd43368852a114a952c3a1aeffb2e1e0e6094c5
Shubham <kumar.shubham1312@gmail.com>
2023-05-19 02:38:18 +0530

    update main.rs to use logic.rs

--- rust_app/src/main.rs
+++ rust_app/src/main.rs
@@ -1,25 +1,37 @@
-use lambda_runtime::{run, service_fn, Error, LambdaEvent};
-use std::fmt;
+use lambda_runtime::Error as LambdaError;
+use lambda_runtime::{run, service_fn, LambdaEvent};
+use lib::org_accordproject_helloworld::MyRequest;
 use serde::{Deserialize, Serialize};
-use serde_json::{json, Value};
+use serde_json::{json, Error, Value};
 
+pub mod logic;
+use logic::logic;
 
 /// This is a made-up example. Requests come into the runtime as unicode
 /// strings in json format, which can map to any structure that implements `serde::Deserialize`
 /// The runtime pays no attention to the contents of the request payload.
 #[derive(Deserialize, Serialize, Debug)]
 struct Request {
-    command: String,
+    input: String,
 }
 
 /// This is a made-up example of what a response structure may look like.
 /// There is no restriction on what it can be. The runtime requires responses
 /// to be serialized into json. The runtime pays no attention
 /// to the contents of the response payload.
-#[derive(Serialize)]
+#[derive(Serialize, Deserialize, Debug)]
 struct Response {
-    status_code: i32,
-    body: String,
+    output: String,
+}
+
+fn into_valid_response(response: Response) -> Result<Value, Error> {
+    Ok(json!({
+        "isBase64Encoded": false,
+        "statusCode": 200,
+        "headers": { },
+        "body": response.output
+
+    }))
 }
 
 /// This is the main body for the function.
@@ -29,32 +41,23 @@ struct Response {
 /// - https://github.com/aws-samples/serverless-rust-demo/
 async fn function_handler(event: LambdaEvent<Value>) -> Result<Value, Error> {
     let (event, _context) = event.into_parts();
-    
-        let parsed_body: Result<Request, serde_json::Error> = serde_json::from_str(&event["body"].as_str().as_deref().unwrap_or(""));
-        if let Ok(result) = parsed_body {
-           return Ok(json!({
-        
-                "isBase64Encoded": false,
-                "statusCode": 200,
-                "headers": { },
-                "body": format!("Hello World! {}", result.command)
-                
-        }));
+
+    let parsed_body: Request = serde_json::from_value(event["body"].clone()).map_err(Into::into)?;
+
+    let response = {
+        let my_request: MyRequest = serde_json::from_str(&parsed_body.input)?;
+        let my_response = logic(my_request)?;
+        let string_response = serde_json::to_string(&my_response)?;
+        Response {
+            output: string_response,
         }
+    };
 
-
-    Ok(json!({
-        
-            "isBase64Encoded": false,
-            "statusCode": 200,
-            "headers": { },
-            "body": format!("Hello World! {}", event["body"])
-            
-    }))
+    into_valid_response(response)
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Error> {
+async fn main() -> Result<(), LambdaError> {
     tracing_subscriber::fmt()
         .with_max_level(tracing::Level::INFO)
         // disable printing the name of the module in every log line.
