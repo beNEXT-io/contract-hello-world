18361d73caa29e7924d991f40d352817c2318120
Shubham <kumar.shubham1312@gmail.com>
2023-05-19 02:37:55 +0530

    add data.json and logic.rs to rust_app

---
+++ rust_app/src/logic.rs
@@ -0,0 +1,38 @@
+use chrono::{DateTime, Utc};
+use lib::org_accordproject_helloworld::{HelloWorldClause, MyRequest, MyResponse};
+use serde_json::Error;
+
+pub struct Contract(HelloWorldClause);
+
+impl Contract {
+    fn new<'a>(path: &'a str) -> Result<HelloWorldClause, Error> {
+        let Ok(data) = std::fs::read_to_string(path) else {
+            panic!("Unable to read HelloWorldClause from data.json")
+        };
+        let json: HelloWorldClause = serde_json::from_str(&data)?;
+        Ok(json)
+    }
+}
+
+pub struct MyResponseImpl(MyResponse);
+
+impl MyResponseImpl {
+    fn new(output: String, _timestamp: DateTime<Utc>) -> Result<MyResponse, Error> {
+        let _class = "org.accordproject.helloworld.MyResponse".to_owned();
+        Ok(MyResponse {
+            _class,
+            output,
+            _timestamp,
+        })
+    }
+}
+
+pub fn logic(request: MyRequest) -> Result<MyResponse, Error> {
+    let path = "/home/sk/contract-hello-world/rust_app/src/data.json";
+    let contract = Contract::new(path)?;
+
+    MyResponseImpl::new(
+        format!("Hello {} {}", contract.name, request.input),
+        request._timestamp,
+    )
+}
