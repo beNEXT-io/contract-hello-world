de41c3598a50d5ec680264d13b8934442715e95f
Martin Halford <martin@benext.io>
2023-05-24 20:45:40 +1000

    Modify to handle template mapping
    Signed-off-by: Martin Halford <martin@benext.io>

--- rust_app/src/main.rs
+++ rust_app/src/main.rs
@@ -1,67 +1,34 @@
 use lambda_runtime::{run, service_fn, Error, LambdaEvent};
-use std::fmt;
 use serde::{Deserialize, Serialize};
-use serde_json::{json, Value};
+use chrono::Utc;
 
+use lib::org_accordproject_helloworld::*;
 
-/// This is a made-up example. Requests come into the runtime as unicode
-/// strings in json format, which can map to any structure that implements `serde::Deserialize`
-/// The runtime pays no attention to the contents of the request payload.
 #[derive(Deserialize, Serialize, Debug)]
 struct Request {
-    command: String,
+    data: HelloWorldClause,
+    request: MyRequest
 }
 
-/// This is a made-up example of what a response structure may look like.
-/// There is no restriction on what it can be. The runtime requires responses
-/// to be serialized into json. The runtime pays no attention
-/// to the contents of the response payload.
-#[derive(Serialize)]
-struct Response {
-    status_code: i32,
-    body: String,
-}
+async fn function_handler(event: LambdaEvent<Request>) -> Result<MyResponse, Error> {
 
-/// This is the main body for the function.
-/// Write your code inside it.
-/// There are some code example in the following URLs:
-/// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
-/// - https://github.com/aws-samples/serverless-rust-demo/
-async fn function_handler(event: LambdaEvent<Value>) -> Result<Value, Error> {
-    let (event, _context) = event.into_parts();
-    
-        let parsed_body: Result<Request, serde_json::Error> = serde_json::from_str(&event["body"].as_str().as_deref().unwrap_or(""));
-        if let Ok(result) = parsed_body {
-           return Ok(json!({
-        
-                "isBase64Encoded": false,
-                "statusCode": 200,
-                "headers": { },
-                "body": format!("Hello World! {}", result.command)
-                
-        }));
-        }
+    let response = MyResponse {
+        _class: event.payload.data._class.clone(),
+        output: format!("Hello {}! {}", event.payload.data.name, event.payload.request.input),
+        _timestamp: Utc::now(),
+    };
 
-
-    Ok(json!({
-        
-            "isBase64Encoded": false,
-            "statusCode": 200,
-            "headers": { },
-            "body": format!("Hello World! {}", event["body"])
-            
-    }))
+    Ok(response)
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     tracing_subscriber::fmt()
         .with_max_level(tracing::Level::INFO)
-        // disable printing the name of the module in every log line.
         .with_target(false)
-        // disabling time is handy because CloudWatch will add the ingestion time.
         .without_time()
         .init();
 
     run(service_fn(function_handler)).await
 }
+
